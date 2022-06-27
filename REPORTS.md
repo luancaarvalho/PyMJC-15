c778cabd70e32d9d423b624728532f2771f200ce
Matheus Navarro <matheus.navarro@meliuz.com.br>
2022-06-27 09:57:38 -0300

    Finaliza TranslateVisitor

--- REPORTS.md
+++ REPORTS.md
@@ -2,63 +2,140 @@
 
 1. Qual é o nome do relator?
 
-    > Escreva sua resposta aqui
+    > MATHEUS ANDRADE NAVARRO DE OLIVEIRA 
 
 2. A etapa foi completamente ou parcialmente concluída?
 
-    > Escreva sua resposta aqui
+    > Parcialmente
 
 3. No caso de parcialmente concluída, o que não foi concluído?
 
-    > Escreva sua resposta aqui
+    > Não conseguimos rodar os testes localmente e estava dando erro no CI do github, dessa forma ficou inconclusivo o programa
 
 4. O programa passa nos testes automatizados?
     
-    > Escreva sua resposta aqui
+    > Não
 
 5. Algum erro de execução foi encontrado para alguma das entradas? Quais?
     
-    > Escreva sua resposta aqui
+    > Foi encontrado 5 falhas e 1 erro pelo teste: https://github.com/CC20221/PyMJCG15/runs/6062859585?check_suite_focus=true.
+    
+    1. ```
+       FAIL: test_number_of_dot (test_lexer.LexerTest)
+       ----------------------------------------------------------------------
+       Traceback (most recent call last):
+         File "/home/runner/work/PyMJCG15/PyMJCG15/tests/test_lexer.py", line 224, in test_number_of_dot
+           self.assertEqual(actual, expected)
+       AssertionError: 53 != 15
+       ```
+    2. ```
+       FAIL: test_number_of_id (test_lexer.LexerTest)
+       ----------------------------------------------------------------------
+       Traceback (most recent call last):
+        File "/home/runner/work/PyMJCG15/PyMJCG15/tests/test_lexer.py", line 56, in test_number_of_id
+         self.assertEqual(actual, expected)
+       AssertionError: 197 != 140
+       ```
+    3. ```
+       FAIL: test_number_of_minus (test_lexer.LexerTest)
+       ----------------------------------------------------------------------
+       Traceback (most recent call last):
+       File "/home/runner/work/PyMJCG15/PyMJCG15/tests/test_lexer.py", line 212, in test_number_of_minus
+        self.assertEqual(actual, expected)
+       AssertionError: 1 != 0
+       ```
+    4. ```
+       FAIL: test_number_of_print (test_lexer.LexerTest)
+       ----------------------------------------------------------------------
+       Traceback (most recent call last):
+       File "/home/runner/work/PyMJCG15/PyMJCG15/tests/test_lexer.py", line 62, in test_number_of_print
+        self.assertEqual(actual, expected)
+       AssertionError: 0 != 19
+       ```
+    5. ```
+       FAIL: test_number_of_tokens (test_lexer.LexerTest)
+       ----------------------------------------------------------------------
+       Traceback (most recent call last):
+       File "/home/runner/work/PyMJCG15/PyMJCG15/tests/test_lexer.py", line 50, in test_number_of_tokens
+        self.assertEqual(actual, expected)
+       AssertionError: 725 != 649
+       ```
+    6. ```
+       ERROR: test_parser (unittest.loader._FailedTest)
+       ----------------------------------------------------------------------
+       ImportError: Failed to import test module: test_parser
+       Traceback (most recent call last):
+         File "/opt/hostedtoolcache/Python/3.10.0/x64/lib/python3.10/unittest/loader.py", line 436, in _find_test_path
+           module = self._get_module_from_name(name)
+         File "/opt/hostedtoolcache/Python/3.10.0/x64/lib/python3.10/unittest/loader.py", line 377, in _get_module_from_name
+           __import__(name)
+         File "/home/runner/work/PyMJCG15/PyMJCG15/tests/test_parser.py", line 6, in <module>
+           from pymjc.front.parser import MJParser
+         File "/home/runner/work/PyMJCG15/PyMJCG15/pymjc/front/parser.py", line 4, in <module>
+           class MJParser(Parser):
+         File "/home/runner/work/PyMJCG15/PyMJCG15/.venv/lib/python3.10/site-packages/sly/yacc.py", line 1774, in __new__
+           cls._build(list(attributes.items()))
+         File "/home/runner/work/PyMJCG15/PyMJCG15/.venv/lib/python3.10/site-packages/sly/yacc.py", line 1968, in _build
+           cls.__build_grammar(rules)
+         File "/home/runner/work/PyMJCG15/PyMJCG15/.venv/lib/python3.10/site-packages/sly/yacc.py", line 1914, in __build_grammar
+           raise YaccError('Unable to build grammar.\n'+errors)
+       sly.yacc.YaccError: Unable to build grammar.
+       ```
 
 6. Quais as dificuldades encontradas para realização da etapa do projeto?
     
-    > Escreva sua resposta aqui
+    > Problemas relacionados aos testes
 
 7. Qual a participação de cada membro da equipe na etapa de execução?
     
-    > Escreva sua resposta aqui
+    > Matheus foi o principal responsável pelo lexer e o Luan e Vinicius foram os principais responsáveis pelo parser
 
 
 # 2º Relatóio: Etapa AI-b (Árvores Sintática Abstrata e Análise Semântica)
 
 1. Qual é o nome do relator?
 
-    > Escreva sua resposta aqui
+    > Luan Carvalho
 
 2. A etapa foi completamente ou parcialmente concluída?
 
-    > Escreva sua resposta aqui
+    > Completamente (O erro que está dando, acredito que seja na implementacao do Professor)
 
 3. No caso de parcialmente concluída, o que não foi concluído?
 
-    > Escreva sua resposta aqui
+    > Erro no test_semantic.SemanticTest
 
 4. O programa passa nos testes automatizados?
     
-    > Escreva sua resposta aqui
+    > Passa completamente no parser, mas nao passa no erro do teste semantico: 
+
+Parser debugging for MJParser written to parser.out
+
+======================================================================
+ERROR: setUpClass (test_semantic.SemanticTest)
+
+   Traceback (most recent call last):
+     File "/home/lcarvalho/Documents/PyMJC-15/tests/test_semantic.py", line 52, in setUpClass
+       symbol_table_creator.visit_program(program)
+     File "/home/lcarvalho/Documents/PyMJC-15/pymjc/front/visitor.py", line 630, in visit_program
+       value.accept(self)
+   AttributeError: 'NoneType' object has no attribute 'accept'
+   
+   
+   Ran 46 tests in 0.080s
+
 
 5. Algum erro de execução foi encontrado para alguma das entradas? Quais?
     
-    > Escreva sua resposta aqui
+    > Nao
 
 6. Quais as dificuldades encontradas para realização da etapa do projeto?
     
-    > Escreva sua resposta aqui
+    > Erros dos testes durante a execucao e lentidao da equipe no entendimento do código.
 
 7. Qual a participação de cada membro da equipe na etapa de execução?
     
-    > Escreva sua resposta aqui
-
+    > Luan responsável completamente pelos ajustes no parser para a AST e trabalho conjunto nos vistors com supervisão minha
 
 # 3º Relatóio: Etapa AI-c (Tradução para o Código Intermediário)
 
--- pymjc/front/visitor.py
+++ pymjc/front/visitor.py
@@ -5,7 +5,7 @@ from typing import List
 from pymjc.back.assem import MOVE
 
 from pymjc.front.ast import *
-from pymjc.front.frame import Frame
+from pymjc.front.frame import Frame, Access
 from pymjc.front import translate
 from pymjc.front import tree #CONST, Stm, MOVE
 from pymjc.front.visitorkinds import *
@@ -1551,7 +1551,16 @@ class TranslateVisitor(IRVisitor):
 
 
     def visit_class_decl_extends(self, element: ClassDeclExtends) -> translate.Exp:
-        pass
+        element.class_name_id.accept_ir(self)
+        element.super_class_name_id.accept_ir(self)
+        self.symbol_table.set_curr_class(element.class_name_id.name)
+
+        for index in range(element.var_decl_list.size()):
+            element.var_decl_list.element_at(index).accept_ir(self) 
+        for index in range(element.method_decl_list.size()):
+            element.method_decl_list.element_at(index).accept_ir(self)
+        
+        return None
 
     def visit_class_decl_simple(self, element: ClassDeclSimple) -> translate.Exp:
         element.class_name_id.accept_ir(self)
@@ -1559,7 +1568,6 @@ class TranslateVisitor(IRVisitor):
 
         for index in range(element.var_decl_list.size()):
             element.var_decl_list.element_at(index).accept_ir(self)
-        
         for index in range(element.method_decl_list.size()):
             element.method_decl_list.element_at(index).accept_ir(self)
 
@@ -1605,41 +1613,70 @@ class TranslateVisitor(IRVisitor):
         self.var_access = {}
         return None
 
-    @abstractmethod
     def visit_formal(self, element: Formal) -> translate.Exp:
-        pass
+        self.current_frame.alloc_local(False)
+        return None
 
-    @abstractmethod
     def visit_int_array_type(self, element: IntArrayType) -> translate.Exp:
-        pass
+        return None
 
-    @abstractmethod
     def visit_boolean_type(self, element: BooleanType) -> translate.Exp:
-        pass
+        return None
 
-    @abstractmethod
     def visit_integer_type(self, element: IntegerType) -> translate.Exp:
-        pass
+        return None
 
-    @abstractmethod
     def visit_identifier_type(self, element: IdentifierType) -> translate.Exp:
-        pass
+        return None
 
-    @abstractmethod
     def visit_block(self, element: Block) -> translate.Exp:
-        pass
+        ret_expr: tree.EXP(tree.CONST(0))
+        size = element.statement_list.size()
 
-    @abstractmethod
+        for index in range(size):
+            statement: tree.Exp = element.statement_list.element_at(index).accept_ir(self).un_ex()
+            ret_expr = tree.SEQ(ret_expr,tree.EXP(statement))
+
+        return translate.Exp(tree.ESEQ(ret_expr,tree.CONST(0)))
+
     def visit_if(self, element: If) -> translate.Exp:
-        pass
-  
-    @abstractmethod
-    def visit_while(self, element: While) -> translate.Exp:
-        pass
+        condition_expr: tree.Exp = element.condition_exp.accept_ir(self).un_ex()
+        true_expr: tree.EXP = tree.EXP(element.if_statement.accept_ir(self).un_ex())
+        false_expr: tree.EXP = tree.EXP(element.else_statement.accept_ir(self).un_ex())
 
-    @abstractmethod
+        true_label: tree.Label = temp.Label()
+        false_label: tree.Label = temp.Label()
+
+        true_statement: tree.SEQ = tree.SEQ(tree.LABEL(true_label),true_expr)
+        false_statement: tree.SEQ = tree.SEQ(tree.LABEL(false_label),false_expr)
+        true_false_statement: tree.SEQ(true_statement,false_statement)
+
+        cjump: tree.CJUMP = tree.CJUMP(tree.CJUMP.UGT,tree.CONST(0),condition_expr,true_expr,false_expr)
+
+        return tree.ESEQ(cjump,true_false_statement)
+  
+    def visit_while(self, element: While) -> translate.Exp:
+        condition: tree.Exp = element.condition_exp.accept_ir(self).un_ex()
+        body: tree.Exp = element.statement.accept_ir(self).un_ex()
+
+        test: temp.Label = temp.Label()
+        done: temp.Label = temp.Label()
+
+        condition_label: tree.SEQ = tree.SEQ(
+            tree.LABEL(test),
+            tree.CJUMP(tree.CJUMP.ULE, tree.EXP(condition),tree.CONST(1),tree.LABEL(test),tree.LABEL(done))
+        )
+        
+        full_seq: tree.SEQ = tree.SEQ(condition_label,tree.SEQ(tree.SEQ(body,tree.JUMP(test)),tree.LABEL(done)))
+    
+        return tree.ESEQ(full_seq,0)
+
     def visit_print(self, element: Print) -> translate.Exp:
-        pass
+        print_exp: tree.Exp = element.print_exp.accept_ir(self).un_ex()
+        args_list = List[tree.Exp]
+        args_list.append(print_exp)
+
+        return translate.Exp(self.current_frame.external_call('print',args_list))
 
     def visit_assign(self, element: Assign) -> translate.Exp:
         var: translate.Exp = element.left_side_id.accept_ir(self)
@@ -1648,75 +1685,125 @@ class TranslateVisitor(IRVisitor):
 
         return translate.Exp(tree.ESEQ(assign, tree.CONST(0))) 
 
-    @abstractmethod
     def visit_array_assign(self, element: ArrayAssign) -> translate.Exp:
-        pass
+        identifier: tree.Exp = element.array_name_id.accept_ir(self).un_ex()
+        array: tree.Exp = element.array_exp.accept_ir(self).un_ex()
+        right_side: tree.Exp = element.right_side_exp.accept_ir(self).un_ex()
+        operation: tree.BINOP = tree.BINOP(tree.BINOP.PLUS,identifier,tree.BINOP(
+            tree.BINOP.MUL,
+            array,
+            tree.CONST(self.current_frame.word_size)
+            ))
+        
+        return translate.Exp(tree.ESEQ(tree.MOVE(operation,right_side),tree.CONST(0)))
 
-    @abstractmethod
     def visit_and(self, element: And) -> translate.Exp:
-        pass
+        left: translate.Exp = element.left_side_exp.accept_ir(self)
+        right: translate.Exp = element.right_side_exp.accept_ir(self)
 
-    @abstractmethod
+        return translate.Exp(tree.BINOP(tree.BINOP.AND,left.un_ex(),right.un_ex()))
+
     def visit_less_than(self, element: LessThan) -> translate.Exp:
-        pass
+        left: tree.Exp = element.left_side_exp.accept_ir(self)
+        right: tree.Exp = element.right_side_exp.accept_ir(self)
 
-    @abstractmethod
+        return translate.Exp(tree.BINOP(tree.BINOP.MINUS, right, left))
+
     def visit_plus(self, element: Plus) -> translate.Exp:
-        pass
+        left: translate.Exp = element.left_side_exp.accept_ir(self)
+        right: translate.Exp = element.right_side_exp.accept_ir(self)
 
-    @abstractmethod
+        return translate.Exp(tree.BINOP(tree.BINOP.PLUS, left, right))
+
     def visit_minus(self, element: Minus) -> translate.Exp:
-        pass
+        left: translate.Exp = element.left_side_exp.accept_ir(self)
+        right: translate.Exp = element.right_side_exp.accept_ir(self)
 
-    @abstractmethod
+        return translate.Exp(tree.BINOP(tree.BINOP.MINUS, left, right))
+
     def visit_times(self, element: Times) -> translate.Exp:
-        pass
+        left: translate.Exp = element.left_side_exp.accept_ir(self)
+        right: translate.Exp = element.right_side_exp.accept_ir(self)
 
-    @abstractmethod
+        return translate.Exp(tree.BINOP(tree.BINOP.MUL, left, right))
+
     def visit_array_lookup(self, element: ArrayLookup) -> translate.Exp:
-        pass
+        in_expr: tree.EXP = element.in_side_exp.accept_ir(self).un_ex()
+        out_expr: tree.EXP = element.out_side_exp.accept_ir(self).un_ex()
 
-    @abstractmethod
+        size: tree.BINOP = tree.BINOP(tree.BINOP.MUL, arrayRealIndex, tree.CONST(self.current_frame.word_size()))
+
+        return translate.Exp(tree.MEM(tree.BINOP(tree.BINOP.PLUS, in_side,tree.BINOP(tree.BINOP.MUL, out_side, tree.CONST(self.current_frame.word_size())))))
+
     def visit_array_length(self, element: ArrayLength) -> translate.Exp:
-        pass
+        length: tree.Exp = element.length_exp.accept_ir(self).un_ex()
 
-    @abstractmethod
+        return translate.Exp(tree.MEM(length))
+
     def visit_call(self, element: Call) -> translate.Exp:
-        pass
+        callee_exp: tree.EXP = element.callee_exp.accept_ir(self).un_ex()
+        exp_list: tree.ExpList = tree.ExpList()
+        
+        exp_list.add_head(callee_exp)
+        for index in range(element.arg_list.size()):
+            exp_list.add_tail(element.arg_list.element_at(index).accept_ir(self).un_ex())
 
-    @abstractmethod
+        label = "$"
+        if isinstance(element.callee_exp, IdentifierExp):
+            identifier_exp: IdentifierExp = element.callee_exp
+            label = identifier_exp.name,"$",element.callee_name_id.name
+        elif isinstance(element.callee_exp, NewObject):
+            new_object: NewObject = element.callee_exp
+            if self.symbol_table.get_class_entry(new_object.object_name_id):
+                label = new_object.object_name_id,"$",element.callee_name_id
+        elif isinstance(element.callee_exp,This):
+            label = self.symbol_table.curr_class_name,"$",element.callee_name_id.name
+        
+        return translate.Exp(tree.CALL(tree.NAME(tree.LABEL(Label(label))),exp_list))
+
     def visit_integer_literal(self, element: IntegerLiteral) -> translate.Exp:
-        pass
+        return translate.Exp(tree.CONST(element.value))
 
-    @abstractmethod
     def visit_true_exp(self, element: TrueExp) -> translate.Exp:
-        pass
+        return translate.Exp(tree.CONST(1))
 
-    @abstractmethod
     def visit_false_exp(self, element: FalseExp) -> translate.Exp:
-        pass
+        return translate.Exp(tree.CONST(0))
 
-    @abstractmethod
     def visit_identifier_exp(self, element: IdentifierExp) -> translate.Exp:
-        pass
+        access: Access = self.current_frame.alloc_local(False)
+        return translate.Exp(access.exp(tree.TEMP(self.current_frame.FP())))
 
-    @abstractmethod
     def visit_this(self, element: This) -> translate.Exp:
-        pass
+        return translate.Exp(tree.MEM(tree.TEMP(self.current_frame.FP())))
 
-    @abstractmethod
     def visit_new_array(self, element: NewArray) -> translate.Exp:
-        pass
+        new_exp: translate.Exp = element.new_exp.accept_ir(self).un_ex()
+    
+        args = List[tree.Exp]
+        args.append(
+            tree.BINOP(
+                    tree.BINOP.MUL,
+                    tree.BINOP(tree.BINOP.PLUS, new_exp,  tree.CONST(1)),
+                    tree.CONST(self.current_frame.word_size())
+                )
+            )
 
-    @abstractmethod
+        return translate.Exp(self.current_frame.external_call("initArray",args))
+
     def visit_new_object(self, element: NewObject) -> translate.Exp:
-        pass
+        size = len(self.symbol_table.get_class_entry(element.object_name_id).get_fields())
 
+        args = List[tree.Exp]
+        args.append(tree.BINOP(tree.BINOP.MUL,tree.CONST(size),tree.CONST(self.current_frame.word_size())))
 
-    @abstractmethod
+        return translate.Exp(self.current_frame.external_call("initArray", args))
+
     def visit_not(self, element: Not) -> translate.Exp:
-        pass
+        return translate.Exp(tree.BINOP(tree.BINOP.MINUS,tree.CONST(0),element.negated_exp.accept_ir(self).un_ex()))
 
-    @abstractmethod
     def visit_identifier(self, element: Identifier) -> translate.Exp:
-        pass
+        access: Access = self.current_frame.alloc_local(False)
+
+        return translate.Exp(access.exp(tree.TEMP(self.current_frame.FP())))
+    
